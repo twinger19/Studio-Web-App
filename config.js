acf99bea4bf3e4ce88b16c5e523132e9f8b6314d
Trevor Wills <mr.twills@gmail.com>
2026-05-02 09:47:51 -0500

    Deploy runtime config so other devices can sync via Supabase

---
+++ config.js
@@ -0,0 +1,5 @@
+window.STUDIO_CONFIG = {
+  SUPABASE_URL: 'https://gzbyncxpmwtpwaegfrnx.supabase.co',
+  SUPABASE_KEY: 'sb_publishable_RYHhWPfpm-ps5TSF3R_qDQ_YEcxilIq',
+  PWD_HASH: '7a092f801daaa8f90ff8449431dd2509b6d0365b18e32fd82b499a6a8d60310e'
+}
